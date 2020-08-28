6eb1daa7738b682481192737c621c62573167e92
Sarah Clark <sarahclark@google.com>
2020-08-28 09:31:50 -0700

    First experiment

--- example/docx_extractor_example.dart
+++ example/docx_extractor_example.dart
@@ -1,6 +1,18 @@
-import 'package:docx_extractor/docx_extractor.dart';
+import 'package:xml/xml_events.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final docXML = '''<w:document>
+    <w:body>
+        <w:p>
+            <w:r>
+                <w:t>Hello, World</w:t>
+            </w:r>
+        </w:p>
+    </w:body>
+</w:document>''';
+
+  Stream.value(docXML)
+      .toXmlEvents()
+      .selectSubtreeEvents((event) => event.localName == 'body')
+      .forEach((event) => print(event));
 }
