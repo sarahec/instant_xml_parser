7bbe44c81b6323b1cd87336ce1e815e5fa2af707
Sarah Clark <sarahclark@google.com>
2020-09-15 15:00:22 -0700

    Simplify annotations

--- parse_tools/lib/annotations.dart
+++ parse_tools/lib/annotations.dart
@@ -1,24 +1,16 @@
 library annotations;
 
-class Tag with TagInfo {
+class Tag {
   final String tag;
   final String method;
 
   const Tag(this.tag, {this.method}) : assert(tag != null, 'tag required');
 }
 
-class Attribute with TagInfo {
-  @override
-  final String tag;
+class Attribute {
   final String attribute;
-  @override
-  final String method;
-  final String match;
+  final String tag;
+  final String matches;
 
-  Attribute(this.attribute, {this.tag, this.method, this.match});
-}
-
-mixin TagInfo {
-  String get tag;
-  String get method;
+  Attribute(this.attribute, {this.tag, this.matches});
 }
