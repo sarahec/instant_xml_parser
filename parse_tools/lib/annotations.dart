f73959e3a2640f05eec5f135f2f2d7d433605e18
Sarah Clark <sarahclark@google.com>
2020-09-16 19:42:37 -0700

    Clarify boolean options, add documentation

--- parse_tools/lib/annotations.dart
+++ parse_tools/lib/annotations.dart
@@ -1,5 +1,7 @@
 library annotations;
 
+import 'package:meta/meta.dart';
+
 class Tag {
   final String tag;
   final String method;
@@ -7,12 +9,29 @@ class Tag {
   const Tag(this.tag, {this.method}) : assert(tag != null, 'tag required');
 }
 
+/// Annotates a field inside a "tag" class to specify the attribute (and tag
+/// if not in the parent tag.)
+///
+/// Notes:
+/// * ```attribute``` is required
+/// * ```tag``` is optional. If you supply it, the generated code will look for
+///   that tag as a child and extract the attribute from there.
+/// * Fill in ```equals``` or ```matches``` when annotating a boolean field.
+@immutable
 class UseAttribute {
+  /// Name for the attribute to extract
   final String attribute;
-  final String tag;
-  final String matches;
 
-  UseAttribute(this.attribute, {this.tag, this.matches})
+  /// The tag name, if not an attribute on the parent tag
+  final String tag;
+
+  /// If this is a boolean field, true when the attribute equals this value
+  final String equals;
+
+  /// If this is a boolean field, true when the attribute matches this regexp
+  final RegExp matches;
+
+  UseAttribute(this.attribute, {this.tag, this.equals, this.matches})
       : assert(attribute != null, 'attribute required');
 }
 
