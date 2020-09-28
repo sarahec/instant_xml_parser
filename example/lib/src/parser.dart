43d488021888cc87cb1e1123b668b807b27f5995
Sarah Clark <sarahclark@google.com>
2020-09-28 14:06:45 -0700

    Design for child parsing

--- example/lib/src/parser.dart
+++ example/lib/src/parser.dart
@@ -5,8 +5,14 @@ import 'package:xml/xml_events.dart';
 
 import 'structures.dart';
 
+const AttributesTagName = 'attributesTest';
+const EmptyTagName = 'emptyTag';
+const NameTagName = 'identification';
+const RegistrationTagName = 'registration';
+const ContactInfoTagName = 'ContactInfo';
+
 Future<AttributesTag> extractAttributesTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: 'attributesTest', rejectOthers: true);
+  final startTag = startOf(events, name: AttributesTagName, rejectOthers: true);
 
   final name = await namedAttribute(startTag, 'name', isRequired: true);
   final count = await namedAttribute(startTag, 'count',
@@ -22,38 +28,45 @@ Future<AttributesTag> extractAttributesTag(StreamQueue<XmlEvent> events) async {
 
 // @Tag('empty', useStrict: true)
 Future<EmptyTag> extractEmptyTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: 'emptyTag', rejectOthers: true);
+  final startTag = startOf(events, name: EmptyTagName, rejectOthers: true);
   await endOf(startTag, events);
   return EmptyTag();
 }
 
 // @Tag('identification')
 Future<NameTag> extractNameTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: 'identification', rejectOthers: true);
+  final startTag = startOf(events, name: NameTagName, rejectOthers: true);
   final name = await namedAttribute(startTag, 'name');
   await endOf(startTag, events);
   return NameTag(await name);
 }
 
-/*
 // @Tag('registration')
 Future<Registration> extractRegistration(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: 'registration', rejectOthers: true);
+  final startTag =
+      startOf(events, name: RegistrationTagName, rejectOthers: true);
+  final age = await namedAttribute(startTag, 'name');
 
-  var nameTag =
-      childOf(startTag, events, name: 'identification', action: extractNameTag);
-  var contact = childOf(events,
-      name: 'ContactInfo', parent: startTag, action: extractContacts);
-  final age = namedAttribute(startTag, 'name');
+  // Process child tags
+  final parsers = {
+    NameTagName: extractNameTag,
+    ContactInfoTagName: extractContacts
+  };
+  final childParser = ChildParser(events, parsers);
+  final resultMap = childParser.parseAll(events);
+  // TODO get the skipped child list
+  final nameTag = await resultMap[NameTagName];
+  final contact = await resultMap[ContactInfoTagName];
+  // TODO Extract any child attributes here
 
   await endOf(startTag, events);
-  return Registration(await nameTag, await contact, await age);
+  return Registration(nameTag, contact, age);
 }
-*/
 
 // @Tag('ContactInfo')
 Future<ContactInfo> extractContacts(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: 'ContactInfo', rejectOthers: true);
+  final startTag =
+      startOf(events, name: ContactInfoTagName, rejectOthers: true);
   final email = await namedAttribute(startTag, 'email');
   final phone = await namedAttribute(startTag, 'phone');
 
