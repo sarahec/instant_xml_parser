cbab546529b32385b5ebaaa0a57c2358870afb89
Sarah Clark <sarahclark@google.com>
2020-09-28 22:57:30 -0700

    Wrap utilities in runtime object for re-entrancy

--- example/lib/src/parser.dart
+++ example/lib/src/parser.dart
@@ -12,63 +12,70 @@ const RegistrationTagName = 'registration';
 const ContactInfoTagName = 'ContactInfo';
 
 Future<AttributesTag> extractAttributesTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: AttributesTagName, mustBeFirst: true);
+  final pr = ParserRuntime();
+  final startTag =
+      pr.startOf(events, name: AttributesTagName, mustBeFirst: true);
 
-  final name = await namedAttribute(startTag, 'name', isRequired: true);
-  final count = await namedAttribute(startTag, 'count',
+  final name = await pr.namedAttribute(startTag, 'name', isRequired: true);
+  final count = await pr.namedAttribute(startTag, 'count',
       convert: Convert.toInt, defaultValue: 0);
-  final temperature =
-      await namedAttribute(startTag, 'temperature', convert: Convert.toDouble);
+  final temperature = await pr.namedAttribute(startTag, 'temperature',
+      convert: Convert.toDouble);
   final active =
-      await namedAttribute(startTag, 'active', convert: Convert.toBool);
+      await pr.namedAttribute(startTag, 'active', convert: Convert.toBool);
 
-  await endOf(startTag, events);
+  await pr.endOf(startTag, events);
   return AttributesTag(name, count, temperature, active);
 }
 
 // @Tag('empty', useStrict: true)
 Future<EmptyTag> extractEmptyTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: EmptyTagName, mustBeFirst: true);
-  await endOf(startTag, events);
+  final pr = ParserRuntime();
+  final startTag = pr.startOf(events, name: EmptyTagName, mustBeFirst: true);
+  await pr.endOf(startTag, events);
   return EmptyTag();
 }
 
 // @Tag('identification')
 Future<NameTag> extractNameTag(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: NameTagName, mustBeFirst: true);
-  final name = await namedAttribute(startTag, 'name');
-  await endOf(startTag, events);
+  final pr = ParserRuntime();
+  final startTag = pr.startOf(events, name: NameTagName, mustBeFirst: true);
+  final name = await pr.namedAttribute(startTag, 'name');
+  await pr.endOf(startTag, events);
   return NameTag(await name);
 }
 
 // @Tag('registration')
 Future<Registration> extractRegistration(StreamQueue<XmlEvent> events) async {
+  final pr = ParserRuntime();
   final startTag =
-      startOf(events, name: RegistrationTagName, mustBeFirst: true);
-  final age = await namedAttribute(startTag, 'age', convert: Convert.toInt);
+      pr.startOf(events, name: RegistrationTagName, mustBeFirst: true);
+  final age = await pr.namedAttribute(startTag, 'age', convert: Convert.toInt);
 
   // Process child tags
-  final parsers = {
+  final dispatch = {
     NameTagName: extractNameTag,
     ContactInfoTagName: extractContacts
   };
-  final childParser = ChildParser(parsers);
+  final childParser = ChildParser(dispatch, pr);
   final resultMap = await childParser.parseAll(events);
   // TODO get the skipped child list
   final nameTag = resultMap[NameTagName];
   final contact = resultMap[ContactInfoTagName];
   // TODO Extract any child attributes here
 
-  await endOf(startTag, events);
+  await pr.endOf(startTag, events);
   return Registration(nameTag, contact, age);
 }
 
 // @Tag('ContactInfo')
 Future<ContactInfo> extractContacts(StreamQueue<XmlEvent> events) async {
-  final startTag = startOf(events, name: ContactInfoTagName, mustBeFirst: true);
-  final email = await namedAttribute(startTag, 'email');
-  final phone = await namedAttribute(startTag, 'phone');
+  final pr = ParserRuntime();
+  final startTag =
+      pr.startOf(events, name: ContactInfoTagName, mustBeFirst: true);
+  final email = await pr.namedAttribute(startTag, 'email');
+  final phone = await pr.namedAttribute(startTag, 'phone');
 
-  await endOf(startTag, events);
+  await pr.endOf(startTag, events);
   return ContactInfo(email, phone);
 }
--- runtime/lib/src/utilities.dart
+++ runtime/lib/src/utilities.dart
@@ -8,90 +8,92 @@ import 'converters.dart';
 import 'errors.dart';
 import 'logging.dart';
 
-/// Scans ahead to the next XmlStartElementEvent, setting event queue
-/// to the found element or resetting it
-///
-/// [events] Incoming event queue
-/// [parent] If supplied, returns only children of this node
-/// [name] If supplied, returns only nodes with this qualified name
-Future<XmlStartElementEvent> startOf(StreamQueue<XmlEvent> events,
-    {String name, XmlStartElementEvent parent, mustBeFirst = false}) async {
-  // Scan for a start tag
-  var transaction = events.startTransaction();
-  var queue = transaction.newQueue();
-  while (await queue.hasNext) {
-    var probe = await queue.peek;
-    if (probe is XmlStartElementEvent) {
-      if ((parent == null || probe.parentEvent == parent) &&
-          (name == null || probe.qualifiedName == name)) {
-        transaction.commit(queue);
-        log.v('Found start of $probe');
-        return probe;
-      } else if (mustBeFirst) {
-        // found, no match, reject
-        transaction.reject();
-        return Future.error(MissingStartTag(name, foundTag: probe.name));
+class ParserRuntime {
+  /// Scans ahead to the next XmlStartElementEvent, setting event queue
+  /// to the found element or resetting it
+  ///
+  /// [events] Incoming event queue
+  /// [parent] If supplied, returns only children of this node
+  /// [name] If supplied, returns only nodes with this qualified name
+  Future<XmlStartElementEvent> startOf(StreamQueue<XmlEvent> events,
+      {String name, XmlStartElementEvent parent, mustBeFirst = false}) async {
+    // Scan for a start tag
+    var transaction = events.startTransaction();
+    var queue = transaction.newQueue();
+    while (await queue.hasNext) {
+      var probe = await queue.peek;
+      if (probe is XmlStartElementEvent) {
+        if ((parent == null || probe.parentEvent == parent) &&
+            (name == null || probe.qualifiedName == name)) {
+          transaction.commit(queue);
+          log.v('Found start of $probe');
+          return probe;
+        } else if (mustBeFirst) {
+          // found, no match, reject
+          transaction.reject();
+          return Future.error(MissingStartTag(name, foundTag: probe.name));
+        }
       }
+      await queue.skip(1);
     }
-    await queue.skip(1);
+    log.v('Reached end of stream looking for start tag');
+    transaction.reject();
+    return null;
   }
-  log.v('Reached end of stream looking for start tag');
-  transaction.reject();
-  return null;
-}
 
-/// Scans to the end of the starting tag, setting the event queue there.
-///
-Future<bool> endOf(FutureOr<XmlStartElementEvent> startTag,
-    StreamQueue<XmlEvent> events) async {
-  var element = await Future.value(startTag);
-  if (element.isSelfClosing) {
-    return true;
-  }
-  var transaction = events.startTransaction();
-  var queue = transaction.newQueue();
-  while (await queue.hasNext) {
-    var probe = await queue.next;
-    if (probe is XmlEndElementEvent &&
-        probe.qualifiedName == element.qualifiedName) {
-      transaction.commit(queue);
+  /// Scans to the end of the starting tag, setting the event queue there.
+  ///
+  Future<bool> endOf(FutureOr<XmlStartElementEvent> startTag,
+      StreamQueue<XmlEvent> events) async {
+    var element = await Future.value(startTag);
+    if (element.isSelfClosing) {
       return true;
     }
-  }
-  // Hit end of stream and found nothing
-  transaction.reject();
-  log.e('Could not find closing tag for <${element.name}>)');
-  return false;
-}
-
-Future<T> namedAttribute<T>(
-    FutureOr<XmlStartElementEvent> elementFuture, String attributeName,
-    {Converter<T> convert, isRequired = false, T defaultValue}) async {
-  assert(convert != null || !(T is String),
-      'converter required for non-String attributes');
-
-  final element = await Future.value(elementFuture);
-
-  final attribute = element.attributes
-      .firstWhere((a) => a.name == attributeName, orElse: () => null);
-  final value = attribute?.value;
-
-  if (value == null) {
-    if (isRequired) {
-      return Future.error(MissingAttribute(element.name, attributeName));
+    var transaction = events.startTransaction();
+    var queue = transaction.newQueue();
+    while (await queue.hasNext) {
+      var probe = await queue.next;
+      if (probe is XmlEndElementEvent &&
+          probe.qualifiedName == element.qualifiedName) {
+        transaction.commit(queue);
+        return true;
+      }
     }
-    // If we're returning the default value, we can bypass the converter
-    return value ?? defaultValue;
+    // Hit end of stream and found nothing
+    transaction.reject();
+    log.e('Could not find closing tag for <${element.name}>)');
+    return false;
   }
 
-  return convert == null ? value : convert(value);
-}
+  Future<T> namedAttribute<T>(
+      FutureOr<XmlStartElementEvent> elementFuture, String attributeName,
+      {Converter<T> convert, isRequired = false, T defaultValue}) async {
+    assert(convert != null || !(T is String),
+        'converter required for non-String attributes');
 
-void reportUnknownChild(XmlStartElementEvent child,
-    {@required XmlStartElementEvent parent, shouldThrow = false}) {
-  log.w('Unknown child <${child.name} inside <${parent.name}>');
-  if (shouldThrow) {
-    throw (UnexpectedChild(child.name));
+    final element = await Future.value(elementFuture);
+
+    final attribute = element.attributes
+        .firstWhere((a) => a.name == attributeName, orElse: () => null);
+    final value = attribute?.value;
+
+    if (value == null) {
+      if (isRequired) {
+        return Future.error(MissingAttribute(element.name, attributeName));
+      }
+      // If we're returning the default value, we can bypass the converter
+      return value ?? defaultValue;
+    }
+
+    return convert == null ? value : convert(value);
+  }
+
+  void reportUnknownChild(XmlStartElementEvent child,
+      {@required XmlStartElementEvent parent, shouldThrow = false}) {
+    log.w('Unknown child <${child.name} inside <${parent.name}>');
+    if (shouldThrow) {
+      throw (UnexpectedChild(child.name));
+    }
   }
 }
 
@@ -99,22 +101,23 @@ typedef ParseMethod<T> = Future<T> Function(StreamQueue<XmlEvent> events);
 
 class ChildParser {
   final Map<String, ParseMethod> dispatchMap;
+  final ParserRuntime pr;
 
-  ChildParser(this.dispatchMap);
+  ChildParser(this.dispatchMap, this.pr);
 
   Iterable<String> get skippedChildren => _skipped;
 
   var _skipped;
 
   Future<Map<String, dynamic>> parseAll(StreamQueue<XmlEvent> events) async {
-    Map<String, dynamic> results = {};
+    Map<String, dynamic> results = {}; // ignore: omit_local_variable_types
     _skipped = <String>[];
     log.v('parsing children)');
     while (await events.hasNext) {
       var transaction = events.startTransaction();
       var queue = transaction.newQueue();
       await queue.skip(1); // Move off initial start tag
-      var probe = await startOf(queue);
+      var probe = await pr.startOf(queue);
       if (probe == null) {
         transaction.reject();
         return results;
