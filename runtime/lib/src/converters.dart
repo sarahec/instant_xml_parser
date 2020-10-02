822af602be04329e5476c46507a8f464112f6e9b
Sarah Clark <sarahclark@google.com>
2020-10-01 17:14:03 -0700

    Protect converters from null values

--- runtime/lib/src/converters.dart
+++ runtime/lib/src/converters.dart
@@ -4,13 +4,13 @@ class Convert {
   static Converter<String> get identity => (s) => s;
 
   static Converter<bool> get toBool =>
-      (s) => s != null && (s == '1' || s == 'true' || s == 'TRUE');
+      (s) => s ?? (s == '1' || s == 'true' || s == 'TRUE');
 
-  static Converter<double> get toDouble => (s) => double.parse(s);
+  static Converter<double> get toDouble => (s) => s ?? double.parse(s);
 
-  static Converter<int> get toInt => (s) => int.parse(s);
+  static Converter<int> get toInt => (s) => s ?? int.parse(s);
 
   static Converter<bool> ifEquals(value) => (s) => value == s;
 
-  static Converter<bool> ifMatches(regexp) => (s) => regexp.hasMatch(s);
+  static Converter<bool> ifMatches(regexp) => (s) => s ?? regexp.hasMatch(s);
 }
