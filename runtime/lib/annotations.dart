8de367d130dab61dc56e1dd8a24f854dccf9e079
Sarah Clark <sarahclark@google.com>
2020-10-25 11:40:43 -0700

    Remove unused

--- runtime/lib/annotations.dart
+++ runtime/lib/annotations.dart
@@ -1,6 +1,4 @@
-library annotations;
-
-const optional = 'optional';
+library runtime.annotations;
 
 class alias {
   /// Name for the attribute to extract (if not inferred from the field name)
@@ -19,18 +17,6 @@ class ifMatches {
   final String regex;
 
   const ifMatches(this.regex);
-}
-
-class ignore {
-  final Iterable<String> tags;
-
-  const ignore(this.tags);
-}
-
-class parser {
-  final String name;
-
-  const parser(this.name);
 }
 
 class tag {
