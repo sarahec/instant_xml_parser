76364556533e5135ba07bfdec333a4044ce69645
Sarah Clark <sarahclark@google.com>
2020-12-21 21:31:29 -0800

    Document annotations

--- runtime/lib/annotations.dart
+++ runtime/lib/annotations.dart
@@ -14,6 +14,17 @@
 
 library runtime.annotations;
 
+/// Defines the XML attribute name for a field when the attribute name doesn't
+/// match the field name.
+///
+///[name] The fully qualified attribute name.
+///
+/// Example:
+/// ```
+/// class TextSegment {
+///   @alias('xml:space')
+///   final String space;
+/// ```
 class alias {
   /// Name for the attribute to extract (if not inferred from the field name)
   final String name;
@@ -21,32 +32,96 @@ class alias {
   const alias(this.name);
 }
 
+/// Defines the String-to-value conversion for this field.
+///
+/// The runtime converts String to any of the Dart primitives, but needs
+/// explicit code to convert any other types.
+/// [source] The name of a method that takes a single String and returns
+/// the desired type.
+///
+/// Example:
+///```
+/// @tag('loc')
+/// class Location {
+///   @convert('Uri.parse')
+///   final Uri loc;
+///
+///   NameTag(this.loc);
+/// }
+/// ```
 class convert {
   final String source;
 
   const convert(this.source);
 }
 
+/// Defines a ```bool``` that is true when the field contains a certain string.
+///
+/// [value] The string to match (case-sensitive)
+///
+/// Example:
+/// ```
+/// class Circuit {
+///   @ifEquals('on')
+///   bool power;
+/// ```
 class ifEquals {
   final String value;
 
   const ifEquals(this.value);
 }
 
+/// Defines a ```bool``` that is true when the field contains a certain regular
+/// expression.
+///
+/// [value] The regex to match. Using a raw string is recommended.
+///
+/// Example:
+/// ```
+/// class Circuit {
+///   @ifMatches(r'(on|1)')
+///   bool power;
+/// ```
 class ifMatches {
   final String regex;
 
   const ifMatches(this.regex);
 }
 
+/// Specifies the XML tag for a class.
+///
+/// [value] The fully qualified tag name.
+///
+/// Example:
+/// ```
+/// ///```
+/// @tag('loc')
+/// class Location {
+/// ```
 class tag {
   final String value;
 
   const tag(this.value);
 }
 
+/// Denotes a field as coming from the XML text inside the current tag.
+///
+/// Example:
+/// ```dart
+/// @tag('note')
+/// class Note {
+///   @textElement
+///   final String body;
+///
+///   Note(this.body);
+/// }
+/// ```
+/// ```xml
+/// <note>This is a test</node>
+/// ```
 const textElement = TextElement();
 
+/// Use ```@textElement```
 class TextElement {
   const TextElement();
 }
