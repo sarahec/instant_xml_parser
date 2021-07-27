6f7488c864a01d314a645daa39dfc63eab1a8d57
Sarah Clark <sarahclark@google.com>
2021-07-26 17:41:45 -0700

    Upgrade test case to null safety

--- integration_testing/lib/child_tag.dart
+++ integration_testing/lib/child_tag.dart
@@ -1,4 +1,3 @@
-// @dart=2.11
 import 'package:ixp_runtime/annotations.dart';
 
 // Copyright 2021 Google LLC
@@ -17,8 +16,8 @@ import 'package:ixp_runtime/annotations.dart';
 
 @tag('contact')
 class ContactInfo {
-  final String email;
-  final String phone;
+  final String? email;
+  final String? phone;
 
   const ContactInfo([this.email, this.phone]);
 }
